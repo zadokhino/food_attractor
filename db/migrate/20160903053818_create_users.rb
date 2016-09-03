a8219436e0009e0852cd23088170b21671117863
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-03 11:38:57 +0600

    Created model User

---
+++ db/migrate/20160903053818_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :phone
+      t.string :address
+      t.boolean :admin
+
+      t.timestamps null: false
+    end
+  end
+end
