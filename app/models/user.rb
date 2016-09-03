8b0fd4976e993657d15130220758ab06b702bb9b
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-03 15:03:23 +0600

    Edited divise views for bootstrap style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	devise :database_authenticatable, :registerable,
+	       :recoverable, :rememberable, :trackable, :validatable
+	validates :name, presence: true, length: {maximum: 50}
+	validates :phone, presence: true, length: {minimum: 6, maximum: 15}
+	validates :address, presence: true, length: {maximum: 50}
 end
