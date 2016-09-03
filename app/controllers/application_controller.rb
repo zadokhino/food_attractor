f0ba737d189fbd374e2cb19951950ea551dc13ed
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-03 12:07:45 +0600

    Added permit params for devise in Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
   	def access_denied(exception)
 		flash[:danger] = exception.message
 		redirect_to root_url
+	end
+
+	private
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :address])
 	end
 end
